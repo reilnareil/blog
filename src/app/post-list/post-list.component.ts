4d322c877144f64849e085f4b20372d68f337eaa
Reilan Cad <reilancadubla@gmail.com>
2023-09-18 06:45:26 +0800

    made post dynamic

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -9,7 +9,9 @@ import { Post } from '../post.model';
 export class PostListComponent implements OnInit {
 
   listOfPosts: Post[] = [
-    new Post("Tech Crunch", "TechCrunch is a blog that provides technology and startup news, from the latest developments in Silicon Valley to venture capital funding. The blog’s target audience is technology and business enthusiasts, especially startup founders and investors worldwide.", "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/12/techcrunch-website-homepage-1024x542.webp", "Reilan", new Date())
+    new Post("Tech Crunch", "TechCrunch is a blog that provides technology and startup news, from the latest developments in Silicon Valley to venture capital funding. The blog’s target audience is technology and business enthusiasts, especially startup founders and investors worldwide.", "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/12/techcrunch-website-homepage-1024x542.webp", "Reilan", new Date()),
+    new Post("Tech Crunc", "TechCrunch is a blog that provides technology and startup news, from the latest developments in Silicon Valley to venture capital funding. The blog’s target audience is technology and business enthusiasts, especially startup founders and investors worldwide.", "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/12/techcrunch-website-homepage-1024x542.webp", "Reilan", new Date()),
+
   ]
 
   constructor() { }
