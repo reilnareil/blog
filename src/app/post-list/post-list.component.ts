5808f84c031e51f59f4f8f429091c9ba90d31e2d
Reilan Cad <reilancadubla@gmail.com>
2023-09-17 16:04:44 +0800

    added post.model.ts

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
+
+  listOfPosts: Post[] = [
+    new Post("Tech Crunch", "TechCrunch is a blog that provides technology and startup news, from the latest developments in Silicon Valley to venture capital funding. The blog’s target audience is technology and business enthusiasts, especially startup founders and investors worldwide.", "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/12/techcrunch-website-homepage-1024x542.webp", "Reilan", new Date())
+  ]
 
   constructor() { }
 
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
+  @Input() post?: Post;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.post)
   }
 
 }
